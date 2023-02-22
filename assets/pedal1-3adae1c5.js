3ecc66d9e6b254b801b2af80e4d0d1952535031f
cmachin <carlos.machin@gmail.com>
2023-02-22 04:37:25 -0400

    Updates

---
+++ assets/pedal1-3adae1c5.js
@@ -0,0 +1,1 @@
+const a="/portfolio/assets/pedal1-f789caaa.jpg";export{a as p};
---
+++ assets/sketch1-8d8ae0bc.js
@@ -0,0 +1,1 @@
+const s="/portfolio/assets/creo5-e4188a0c.png",o="/portfolio/assets/sketch1-bf0debc4.jpg";export{s as c,o as s};
