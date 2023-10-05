0703b7b855758b55b2fcd191002ca47fe981cedc
cmachin <carlos.machin@gmail.com>
2023-10-04 22:36:42 -0500

    Updates

---
+++ assets/Certifications-1fca30f1.js
@@ -0,0 +1,1 @@
+import{j as i,i as t,C as e}from"./index-03d9a7d9.js";import{b as s,R as n}from"./rest.api-491bed2b.js";function o(){return i("div",{className:"certifications",children:s().map((a,r)=>t(e,{children:[i("h1",{children:a.title}),i(n,{className:"item-cards",children:i("a",{href:a.link,target:"_blank",children:i("img",{src:a.image,alt:a.title})})})]},r))})}export{o as default};
--- assets/pedal1-3adae1c5.js
+++
@@ -1,1 +0,0 @@
-const a="/portfolio/assets/pedal1-f789caaa.jpg";export{a as p};
